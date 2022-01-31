47dd3a7f974d73cbb00018f0b8161f7f1a30595e
Pyotr Petkov <pyotr.petkov@gmail.com>
2022-01-31 21:55:09 +0200

    add api constants

---
+++ src/constants/api.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://openlibrary.org/'
+export const BOOKS_URL = `${BASE_URL}isbn/`
+export const BOOK_SEARCH_URL = `${BASE_URL}search.json?q=`
+export const AUTHOR_SEARCH_URL = `${BASE_URL}search.json?author=`
