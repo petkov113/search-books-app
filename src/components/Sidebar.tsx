e262f66e43a7100c728026b2b45cac51ec00e1ce
Pyotr Petkov <pyotr.petkov@gmail.com>
2022-02-12 00:49:03 +0200

    add a tooltip

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { QuestionOutlineIcon, SearchIcon } from '@chakra-ui/icons'
-import { Center, Circle, GridItem, VStack } from '@chakra-ui/react'
+import { Center, Circle, GridItem, Tooltip, VStack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 const linkHoverStyles = { backgroundColor: 'teal.500', transition: 'all .2s' }
@@ -8,26 +8,42 @@ const Sidebar = () => {
   return (
     <GridItem as="nav" bg="gray.700" pt={3}>
       <VStack as="ul">
-        <Link to="/">
-          <Circle
-            size="40px"
-            bg="teal.700"
-            color="white"
-            _hover={linkHoverStyles}
-          >
-            <SearchIcon />
-          </Circle>
-        </Link>
-        <Link to="/about">
-          <Circle
-            size="40px"
-            bg="teal.700"
-            color="white"
-            _hover={linkHoverStyles}
-          >
-            <QuestionOutlineIcon />
-          </Circle>
-        </Link>
+        <Tooltip
+          label="Search books"
+          fontSize="md"
+          placement="right"
+          bg="teal"
+          hasArrow
+        >
+          <Link to="/">
+            <Circle
+              size="40px"
+              bg="teal.700"
+              color="white"
+              _hover={linkHoverStyles}
+            >
+              <SearchIcon />
+            </Circle>
+          </Link>
+        </Tooltip>
+        <Tooltip
+          label="About"
+          fontSize="md"
+          placement="right"
+          bg="teal"
+          hasArrow
+        >
+          <Link to="/about">
+            <Circle
+              size="40px"
+              bg="teal.700"
+              color="white"
+              _hover={linkHoverStyles}
+            >
+              <QuestionOutlineIcon />
+            </Circle>
+          </Link>
+        </Tooltip>
       </VStack>
     </GridItem>
   )
