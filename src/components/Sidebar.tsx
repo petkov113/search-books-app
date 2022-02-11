00d9ee7634a080b943a7281ffe9d5ce0b5fb9624
Pyotr Petkov <pyotr.petkov@gmail.com>
2022-02-12 00:01:29 +0200

    add sidebar links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,36 @@
-import { GridItem } from '@chakra-ui/react'
-import React from 'react'
+import { QuestionOutlineIcon, SearchIcon } from '@chakra-ui/icons'
+import { Center, Circle, GridItem, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
+const linkHoverStyles = { backgroundColor: 'teal.500', transition: 'all .2s' }
 
 const Sidebar = () => {
-  return <GridItem bg="gray.700"></GridItem>
+  return (
+    <GridItem as="nav" bg="gray.700" pt={3}>
+      <VStack as="ul">
+        <Link to="/">
+          <Circle
+            size="40px"
+            bg="teal.700"
+            color="white"
+            _hover={linkHoverStyles}
+          >
+            <SearchIcon />
+          </Circle>
+        </Link>
+        <Link to="/about">
+          <Circle
+            size="40px"
+            bg="teal.700"
+            color="white"
+            _hover={linkHoverStyles}
+          >
+            <QuestionOutlineIcon />
+          </Circle>
+        </Link>
+      </VStack>
+    </GridItem>
+  )
 }
 
 export default Sidebar
